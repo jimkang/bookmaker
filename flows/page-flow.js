871a9e00f7af4991c1cb78d353dc39fc3d742dd2
Jim Kang <jimkang@gmail.com>
2018-11-24 18:00:45 -0500

    Create NxN graph connecting all points.

--- flows/page-flow.js
+++ flows/page-flow.js
@@ -2,6 +2,8 @@ var seedrandom = require('seedrandom');
 var Probable = require('probable').createProbable;
 var range = require('d3-array').range;
 var renderJoints = require('../dom/render-joints');
+var renderEdges = require('../dom/render-edges');
+var math = require('basic-2d-math');
 
 function PageFlow({ seed }) {
   var random = seedrandom(seed);
@@ -39,11 +41,36 @@ function PageFlow({ seed }) {
   }
 
   function boneStep() {
-    console.log('bone', probable.roll(6));
+    var graph = getNByNGraph({ points: page.joints });
+    console.log(graph);
+    renderEdges({ edges: graph, className: 'n-by-n-edge', rootSelector: '#n-by-n-graph' });
   }
 
   function getRandomPoint() {
     return [probable.roll(1000)/10, probable.roll(1000)/10];
+  }
+}
+
+// Creates edges between every point and every other point in points.
+function getNByNGraph({ points }) {
+  var graph = [];
+  points.map(createEdgesToOtherPoints);
+  return graph;
+
+  function createEdgesToOtherPoints(point, i) {
+    for (var j = 0; j < points.length; ++j) {
+      if (i !== j) {
+        graph.push({
+          start: i,
+          dest: j,
+          x1: point[0],
+          y1: point[1],
+          x2: points[j][0],
+          y2: points[j][1],
+          dist: math.getVectorMagnitude(point, points[j])
+        });
+      }
+    }
   }
 }
 
